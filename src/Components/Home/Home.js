4edc186dc7bd0b77bf52c99c6400725f7399a848
SouravBiswas238 <souravbiswas427@gmail.com>
2022-04-04 04:39:03 +0600

    adding ratting section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Reviews from '../Reviews/Reviews';
+
+
 import './Home.css';
 
 const Home = () => {
--- src/Components/SingleReview/SingleReview.js
+++ src/Components/SingleReview/SingleReview.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import './SingleReview.css'
+import Rating from 'react-rating';
+import './SingleReview.css';
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SingleReview = (props) => {
-    const { name, picture, email, gender, registered, about } = props.review;
+    const { name, picture, email, rating, registered, about } = props.review;
     return (
         <div >
             <div className='single-review-cart'>
@@ -10,8 +13,15 @@ const SingleReview = (props) => {
                     <img src={picture} alt="" />
                     <span>{email}</span>
                 </div>
-                <p>Using Font- awsome</p>
-                {/* <p>email: {email} </p> */}
+                <p className='fa-icon-color'>
+                    <Rating
+                        initialRating={rating}
+                        emptySymbol={<FontAwesomeIcon icon={faStar} />}
+                        fullSymbol={<FontAwesomeIcon style={{ color: 'goldenrod' }} icon={faStar} />}
+                        readonly
+                    ></Rating>
+                </p>
+
                 <p>{about}</p>
                 <p>By <span>{name}</span> on <small>{registered}
                 </small></p>
