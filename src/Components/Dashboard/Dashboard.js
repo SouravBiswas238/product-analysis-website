8ec94395221ef4bb033c65b16b8a5583a8502a8f
SouravBiswas238 <souravbiswas427@gmail.com>
2022-04-04 14:57:28 +0600

    adding-chart container

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,45 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, BarChart, Legend, Bar } from 'recharts';
+import './Dashboard.css'
 const Dashboard = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, []);
+
     return (
-        <div>
-            <h2>This is dashbord and here show Chart</h2>
+        <div className='chart-container'>
+            <div>
+                <h2>Line Chart For revenue And Investment</h2>
+
+                <LineChart width={900} height={600} data={data}>
+
+                    <Line dataKey={"investment"}></Line>
+                    <Line dataKey={"revenue"}></Line>
+
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <XAxis dataKey="month"></XAxis>
+                    <YAxis ></YAxis>
+                    <Tooltip />
+                </LineChart>
+            </div>
+
+            <div className='bar-chart-container'>
+                <h2>BarChart For Sell</h2>
+                <BarChart width={800} height={500} data={data}>
+                    <XAxis dataKey="month" stroke="#8884d8" />
+                    <YAxis />
+                    <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
+
+                    <Legend width={100} wrapperStyle={{ top: 30, right: 10, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
+
+
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <Bar dataKey="sell" fill="#4D77FF" barSize={50} />
+                </BarChart>
+            </div>
         </div>
     );
 };
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div >
+            <img style={{ width: "100%" }} src="https://media.istockphoto.com/photos/error-page-not-found-picture-id1153372686?b=1&k=20&m=1153372686&s=170667a&w=0&h=4veHHEjI5rq2qpYWMXcHSzXfrp22SMwa79DsqqiYfAw=" alt="404" />
+        </div >
+    );
+};
+
+export default NotFound;
