3a7559b0fb230ad7ee5c5fdbba648c195fee998a
SouravBiswas238 <souravbiswas427@gmail.com>
2022-04-05 00:12:04 +0600

    final Commit

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -10,7 +10,7 @@ const Blogs = () => {
                     It's like a global variable it can be inserted into any component directly. This API solves one major problem props drilling.</p>
             </div>
             <div>
-                <h1>Context Api</h1>
+                <h1>Semantic Tag</h1>
                 <p>A semantic element clearly describes its meaning to the browswe and the developer.there are some simantic tag that use to difine parts of the webpage:</p>
                 <p>
                     <b>Semantic Elements:</b>
